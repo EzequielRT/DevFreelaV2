0f8f922b1150bac54abf6e0a347b6facf45f2591
Ezequiel Trindade <ezefuts@gmail.com>
2025-04-22 22:00:58 -0300

    Adiciona DomainException

---
+++ DevFreela.API/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace DevFreela.API.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
