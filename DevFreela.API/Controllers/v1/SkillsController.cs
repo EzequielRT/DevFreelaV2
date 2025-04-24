4b508b8198cc77a8abd5a455f5e80d617d7de2b1
Ezequiel Trindade <ezefuts@gmail.com>
2025-04-23 23:09:14 -0300

    Adiciona EF Core nas actions da SkillsController

--- DevFreela.API/Controllers/v1/SkillsController.cs
+++ DevFreela.API/Controllers/v1/SkillsController.cs
@@ -1,5 +1,9 @@
-﻿using DevFreela.API.Models.Input;
+﻿using DevFreela.API.Entities;
+using DevFreela.API.Models.Input;
+using DevFreela.API.Models.View;
+using DevFreela.API.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.API.Controllers.v1
 {
@@ -7,15 +11,31 @@ namespace DevFreela.API.Controllers.v1
     [ApiController]
     public class SkillsController : ControllerBase
     {
+        private readonly DevFreelaDbContext _context;
+
+        public SkillsController(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            return Ok();
+            var skills = await _context.Skills.ToListAsync();
+
+            var model = skills.Select(SkillItemViewModel.FromEntity).ToList();
+
+            return Ok(model);
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(CreateSkillInputModel input)
         {
+            var skill = new Skill(input.Description);
+
+            await _context.Skills.AddAsync(skill);
+            await _context.SaveChangesAsync();
+
             return Ok();
         }
     }
---
+++ DevFreela.API/Models/View/SkillItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using DevFreela.API.Entities;
+
+namespace DevFreela.API.Models.View
+{
+    public class SkillItemViewModel
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+
+        public static SkillItemViewModel FromEntity(Skill skill)
+        {
+            return new()
+            {
+                Id = skill.Id,
+                Description = skill.Description
+            };
+        }
+    }
+}
